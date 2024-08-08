3fdab77885da151a9a876ca10546426efb8324bc
Jervicandres <jervic.47@gmail.com>
2024-08-08 16:02:17 +0800

    fix search params error

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@styles/globals.css';
 
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import { Suspense } from 'react';
 
 export const metadata = {
    title: "Promptopia",
@@ -12,16 +13,18 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
-         <Provider>
-         <div className="main">
-            <div className='gradient'/>
-         </div>
+         <Suspense>
+            <Provider>
+            <div className="main">
+               <div className='gradient'/>
+            </div>
 
-         <main className="app">
-            <Nav />
-            {children}
-         </main>
-         </Provider>
+            <main className="app">
+               <Nav />
+               {children}
+            </main>
+            </Provider>
+         </Suspense>
       </body>
     </html>
   )
